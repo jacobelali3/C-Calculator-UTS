842cbe676fd41faa17539220a8b0fbd82aca0aac
Jacob Elali <41930662+jacobelali3@users.noreply.github.com>
2018-09-25 15:37:36 +1000

    Add files via upload

---
+++ MasterBranch.c
@@ -0,0 +1,80 @@
+#include <math.h> 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+askuser (void)
+{
+printf(“You want to run the program as:/n  1.Customer /n 2.admin ”)
+scanf(/*scan the value from user then run the program at next level*/)
+}
+
+Int main (void)
+{
+
+
+
+
+
+ addCustomer()
+{
+
+}
+ 
+
+addPlan()   
+{
+
+
+}
+
+delete plan()
+{
+	
+}
+
+
+saveInfo() 
+
+{
+
+}
+
+readinfo()
+{
+
+}
+
+calculateInterest() 
+{
+	
+}
+
+calculate () 
+{
+
+}
+
+
+displayInfo() 
+{
+
+}
+
+Deleteinfo()
+{
+
+} 
+
+Displayplan()
+{
+
+}
+
+
+
+
+
+ return 0;
+}
